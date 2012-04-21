c0ac1ad0def69700cb0143b5ea65b68ee2354a6f
yuiseki <yuiseki@www7140uf.sakura.ne.jp>
2012-04-21 20:02:52 +0900

    update brain api

--- brain/app.rb
+++ brain/app.rb
@@ -3,22 +3,19 @@ require 'rubygems'
 require 'rack'
 require 'sinatra'
 require 'json'
-require 'serialport'
 
 configure do
-    serial = "/dev/ttyACM0"
-    set :sp, SerialPort.new(serial, 9600)
     set :public_folder, 'public'
     disable :protection
 end
 
 get '/brain' do
     # read brain data from db
-    line = settings.sp.gets
-    puts line
+    puts params
     return Time.now.to_i.to_s+','+line
 end
 
 post '/brain' do
+    puts params
     # write brain data from db
 end
